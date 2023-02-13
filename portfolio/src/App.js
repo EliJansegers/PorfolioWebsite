dc99d1cc7cb447a7e4cae0397035226f1893e19a
EliJansegers <eli.jansegers@student.hogent.be>
2023-02-13 20:54:27 +0100

    Installing app and tailwind
    works

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='text-orange-600 text-8xl'>
+      <header className="App-header">
+        <p className='text-orange-600 text-8xl'>
+          yoooo
+        </p>
+      </header>
+    </div>
+  );
+}
+export default App;
