d43977894b35965603aa73dc8eb85c96d274cba5
Eli Jansegers <eli.jansegers@student.hogent.be>
2023-12-15 14:44:05 +0100

    sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blueGray text-offWhite body-font shadow-lg">
+    <header className="bg-blueGray text-offWhite body-font shadow-lg sticky top-0 z-50">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a href="#about"  className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <span className="ml-3 text-xl text-offWhite">Eli Jansegers</span>
