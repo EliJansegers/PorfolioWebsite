e9101f3323682511dc0d12fb789e6cdd208914f1
Eli Jansegers <eli.jansegers@student.hogent.be>
2023-12-15 12:33:48 +0100

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
     >
     <header className="bg-blueGray text-offWhite body-font shadow-lg">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <a href="#about"  className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <span className="ml-3 text-xl text-offWhite">Eli Jansegers</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base  justify-center">
